19ee84c15a7363e6bc8986c92fba85f21263fc92
Azalea4u <dnguyen@student.neumont.edu>
2024-04-24 18:19:17 -0600

    WIP World Sprite

--- Assets/Sprites/PlayerMovement.cs
+++ Assets/Sprites/PlayerMovement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float speed;
+    [SerializeField] Animator animator;
+
+    private void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical, 0);
+
+        AnimateMovement(direction);
+
+        transform.position += direction * speed * Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AnimateMovement(Vector3 direction)
     {
-        
+        if (animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                animator.SetBool("IsMoving", true);
+
+                animator.SetFloat("Horizontal", direction.x);
+                animator.SetFloat("Vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("IsMoving", false);
+            }
+
+        }
     }
 }
